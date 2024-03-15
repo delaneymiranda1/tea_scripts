d7525dce89d4b518529c85ca0787e63b6f6a402d
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-15 16:14:31 -0600

    add testing for subscription cancellation

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -65,4 +65,34 @@ RSpec.describe "Subscriptions", type: :request do
     
     expect(json[:errors][:title]).to eq("All fields must be filled in.")
   end
+
+  #PATCH /api/v1/subscriptions/subscription_id
+  it "cancels a subscription" do
+    
+    subscription = Subscription.create!(title: "Black Tea Subscription", price: 20, status: 1, frequency: "monthly", tea_id: @tea.id, customer_id: @customer.id)
+
+    headers = { "CONTENT_TYPE" => "application/json",
+      "ACCEPT" => "application/json"
+    }
+
+    canelled = { status: 0 }
+
+    patch "/api/v1/subscriptions/#{subscription.id}", headers: headers, params: cancelled.to_json
+
+    expect(response.status).to eq(200)
+
+    json = JSON.parse(response.body, symbolize_names: true) 
+
+    expect(json).to have_key(:data)
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data]).to have_key(:attributes)
+
+    expect(json[:data][:attributes]).to have_key(:title)
+    expect(json[:data][:attributes]).to have_key(:price)
+    expect(json[:data][:attributes]).to have_key(:status)
+    expect(json[:data][:attributes]).to have_key(:frequency)
+    
+    expect(json[:data][:attributes][:status]).to eq("cancelled")
+  end
 end
